c4cee7141ec049f2de6bbb0006570cee3d5edee5
Cliff Harvey <clifford.harvey@klarna.com>
2021-03-13 16:54:26 +0100

    add Birger's git log shortcut as 'll'

--- home.nix
+++ home.nix
@@ -80,6 +80,7 @@
       d = "diff";
       dc = "diff --cached";
       l = "log --graph --stat";
+      ll = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
       pop = "reset --soft HEAD^";
     };
 
